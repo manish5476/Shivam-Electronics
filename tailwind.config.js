bdb3babfdc7a661bf302b542753a0749858a699e
Manish.Singh <msms5476m@gmail.com>
2025-03-29 21:19:02 +0530

    m

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
+// module.exports = {
+//   theme: {
+//     extend: {},
+//     color: { mode: 'rgb' } // Force Tailwind to use RGB instead of OKLCH
+//   },
+//   corePlugins: {
+//     preflight: false, // Optional: If you face CSS reset issues
+//   },
+//   plugins: [],
+// };
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Include all Angular files
+  ],
   theme: {
     extend: {},
-    color: { mode: 'rgb' } // Force Tailwind to use RGB instead of OKLCH
-  },
-  corePlugins: {
-    preflight: false, // Optional: If you face CSS reset issues
   },
   plugins: [],
 };
