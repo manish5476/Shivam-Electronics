ee739bdc0a010e1c09282a63c55cb91368b11c19
Manish.Singh <msms5476m@gmail.com>
2025-05-20 20:40:59 +0530

    manish

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,15 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
-
-// export const serverRoutes: ServerRoute[] = [
-//   {
-//     path: '**',
-//     renderMode: RenderMode.Prerender
-//   }
-// ];
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'products/detail/:id',
-    renderMode: RenderMode.Server  // 👈 disables prerendering for this route
+    renderMode: RenderMode.Server  
   },
   {
     path: '**',
