d5988e161cbd54de419c081154788565918d3394
manishsingh2002 <msms5476mm@gmail.com>
2025-09-06 10:56:58 +0530

    updated the prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   // apiUrl: 'http://localhost:4002/api'
   // apiUrl: 'https://shivamelectronicsbackend.onrender.com/api'
-  apiUrl: 'https://shivamelectronicsbackendprod.onrender.com/api'
+  // apiUrl: 'https://shivamelectronicsbackendprod.onrender.com/api'
+  apiUrl: 'https://shivamelectronicsbackend-pdsr.onrender.com/api'
 
 };
